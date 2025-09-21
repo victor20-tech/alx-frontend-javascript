edb0c555ea7424b9e1dbf049a727394de01bc75d
victor20-tech <victoradomako10@gmail.com>
2025-09-21 23:09:42 +0000

    feat: Implement Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+//interface Teacher
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+
+
+//object teacher1
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
