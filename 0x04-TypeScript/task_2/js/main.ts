29be9295af1d778bee68ec23f1db8cb1d9752a43
Victor Adomako <victoradomako10@gmail.com>
2025-09-28 11:16:46 -0400

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -48,12 +48,21 @@ type Employee = Director | Teacher;
 
 
 // Create and return an instance of Director or Teacher based on salary.
+// function createEmployee(salary: number | string): Employee {
+//   if (typeof salary === 'number' && salary < 500) {
+//     return new Teacher();
+//   } else {
+//     return new Director();
+//   }
+// }
+
 function createEmployee(salary: number | string): Employee {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  } else {
-    return new Director();
+  if (typeof salary === 'number') {
+    if (salary < 500) { 
+      return new Teacher();
+    }
   }
+  return new Director();
 }
 
 console.log(createEmployee(200));
