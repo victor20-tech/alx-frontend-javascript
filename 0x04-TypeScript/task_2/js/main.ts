fe241df2034614b3ba8f3d3c1056adeb05f9d499
victor20-tech <victoradomako10@gmail.com>
2025-09-28 15:03:22 -0400

    feat: Add interfaces, classes, and factory function for employees

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,61 @@
+//Director Interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+//Teacher Interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Implement the interfaces with specific methods.
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+
+//type that can be either Director or Teacher.
+type Employee = Director | Teacher;
+
+
+// Create and return an instance of Director or Teacher based on salary.
+function createEmployee(salary: number | string): Employee {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
