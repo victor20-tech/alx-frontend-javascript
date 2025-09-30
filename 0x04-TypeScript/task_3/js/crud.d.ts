f9ac14018d226bd50ff49434957dbd06ed585518
Victor Adomako <victoradomako10@gmail.com>
2025-09-30 12:02:00 -0400

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -8,17 +8,17 @@ import { RowID, RowElement } from './interface';
  * Inserts a new row and returns its ID.
  * @param row The data for the new row.
  */
-export function insertRow(row: RowElement): RowID;
+export declare function insertRow(row: RowElement): RowID;
 
 /**
  * Deletes a row by its ID.
  * @param rowId The ID of the row to delete.
  */
-export function deleteRow(rowId: RowID): void;
+export declare function deleteRow(rowId: RowID): void;
 
 /**
  * Updates an existing row.
  * @param rowId The ID of the row to update.
  * @param row The new data for the row.
  */
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
